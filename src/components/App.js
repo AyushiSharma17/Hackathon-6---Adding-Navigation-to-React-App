280368216f5d1751085701a909f76c6e4d1345c7
Ayushi Sharma <sharmaroliayushi@gmail.com>
2020-11-23 21:58:05 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component, useState} from "react";
+import {useLocation, BrowserRouter,Switch, Route} from 'react-router-dom';
 import '../styles/App.css';
 
 class App extends Component {
@@ -6,11 +7,36 @@ class App extends Component {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+            <BrowserRouter>
+            <LocationDisplay />
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            
+            <Switch>
+                <Route exact path="/about">
+                    <div>You are on the about page</div>
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="*">
+                    <div>No match</div>
+                </Route>
+                
+            </Switch>
+            </BrowserRouter>
+               
             </div>
-        )
+        );
     }
 }
+function Home () {
+    return <div>You are home</div>;
+}
 
+function LocationDisplay() {
+    const {pathname} = useLocation();
+    return <div data-testid="location-display" >{pathname}</div> ;
+}
 
 export default App;
